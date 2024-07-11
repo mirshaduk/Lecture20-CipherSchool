7027616f801d335f9d8721fa700be2c803bd1fcb
mirshaduk <119918613+mirshaduk@users.noreply.github.com>
2024-07-11 10:03:28 +0530

    Add files via upload

---
+++ memory1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    long long int a;
+    int b;
+
+    cout<<"The size of int is :"<<sizeof(b)<<"The size of long long int is:"<<sizeof(a);
+    cout<<"The adrees of a is "<<&a<<"The address of b is "<<&b;
+
+    
+    return 0;
+}
